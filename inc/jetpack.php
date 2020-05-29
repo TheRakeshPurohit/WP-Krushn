31a60f3ffdf22bc72139e19178c7f9a77741ebba
therakeshpurohit <rakeshpurohit909@gmail.com>
2020-05-29 09:47:53 +0530

    added boilerplate

---
+++ inc/jetpack.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Krushn
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
+ */
+function divinity_everywhere_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support(
+		'infinite-scroll',
+		array(
+			'container' => 'main',
+			'render'    => 'divinity_everywhere_infinite_scroll_render',
+			'footer'    => 'page',
+		)
+	);
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support(
+		'jetpack-content-options',
+		array(
+			'post-details' => array(
+				'stylesheet' => 'divinity-everywhere-style',
+				'date'       => '.posted-on',
+				'categories' => '.cat-links',
+				'tags'       => '.tags-links',
+				'author'     => '.byline',
+				'comment'    => '.comments-link',
+			),
+			'featured-images' => array(
+				'archive' => true,
+				'post'    => true,
+				'page'    => true,
+			),
+		)
+	);
+}
+add_action( 'after_setup_theme', 'divinity_everywhere_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function divinity_everywhere_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_type() );
+		endif;
+	}
+}
